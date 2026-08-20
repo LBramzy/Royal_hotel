f383e16b43b010809dfe0899b964e9208e60662d
LBramzy <legendarybramzy@gmail.com>
2026-08-20 17:34:26 +0100

    Vite Update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
+import { glob } from 'glob';
 // import tailwindcss from '@tailwindcss/vite';
+
+const images = glob.sync(
+    'resources/images/**/*.{png,jpg,jpeg,svg,gif,webp}',
+    'resources/icon/**/*.{png,jpg,jpeg,svg,gif,webp}'
+);
+
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', ...images,],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
