b7ad92a3068810bf2f6b904a43a7b476e38cb2a5
LBramzy <legendarybramzy@gmail.com>
2026-08-20 18:35:23 +0100

    Vite Manifest

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import { bunny } from 'laravel-vite-plugin/fonts';
+import tailwindcss from '@tailwindcss/vite';
 
 
 
@@ -15,7 +16,7 @@ export default defineConfig({
                 }),
             ],
         }),
-        // tailwindcss(),
+        tailwindcss(),
     ],
     server: {
         watch: {
